c0404a4ad07430e1dc46be56a6190a65cd1cbe3a
AqAshiq <ashiq.ar7@gmail.com>
2023-01-02 08:37:51 -0800

    my task

---
+++ src/Pv.java
@@ -0,0 +1,18 @@
+
+public class Pv {
+	
+	
+	public void m1() {
+    System.out.println("aaaaaaaaa");
+	}
+
+	public void m2() {
+	    System.out.println("bbbbbbbbb");
+		}
+
+	
+	public void m3() {
+	    System.out.println("cccccccc");
+		}
+
+}
